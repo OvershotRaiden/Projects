dea8a7425521f8186ff6eba564ac2b5f085da357
Rubin <mcribling@gmail.com>
2021-05-22 16:20:24 -0500

    bubblesort algorithm and list

---
+++ bubblesort/bubblesort.py
@@ -0,0 +1,9 @@
+def bubble_sort(my_list):
+    for i in range(len(my_list)):
+        for j in range(len(my_list) - 1):
+            if my_list[j] > my_list[j+1]:
+                my_list[j], my_list[j+1] = my_list[j+1], my_list[j]
+
+my_list = [90,17,34,1,0,-40,57,40,-1,66]
+bubble_sort(my_list)
+print(my_list)
